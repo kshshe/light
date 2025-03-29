63ed6c260235c27b14bd1381c865df03c31ea2c8
Aleksei Laud <mail@a-laud.ru>
2025-03-29 23:25:47 +0400

    Add GPU-based rendering of light sources

--- main.js
+++ main.js
@@ -1,20 +1,84 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const gpu = new GPU.GPU();
+const render = gpu.createKernel(function(sources) {
+  const x = this.thread.x;
+  const y = this.thread.y;
 
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  const sourcesCount = sources[0];
+  let sourceX = 0;
+  let sourceY = 0;
+  let sourceIntensity = 0;
+
+  let sumOfIntensities = 0;
+
+  for (let i = 0; i < sourcesCount; i++) {
+    sourceX = sources[1 + i * 3];
+    sourceY = sources[2 + i * 3];
+    sourceIntensity = sources[3 + i * 3];
+    
+    const distance = Math.sqrt((x - sourceX) ** 2 + (y - sourceY) ** 2);
+    const intensity = sourceIntensity / distance;
+    sumOfIntensities += intensity;
+  }
+
+  this.color(sumOfIntensities, sumOfIntensities, sumOfIntensities, 1);
+})
+.setOutput([window.innerWidth, window.innerHeight])
+.setGraphical(true);
+
+const canvas = render.canvas;
+document.getElementsByTagName('body')[0].appendChild(canvas);
+
+const lightSource = {
+  isVisible: false,
+  position: {
+    x: 400,
+    y: 400,
+  },
+  intensity: 10,
 }
 
-resizeCanvas();
+window.addEventListener('mousemove', (e) => {
+  lightSource.isVisible = true;
+  lightSource.position.x = e.clientX;
+  lightSource.position.y = window.innerHeight - e.clientY;
+});
 
-window.addEventListener('resize', resizeCanvas);
+window.addEventListener('mouseleave', () => {
+  lightSource.isVisible = false;
+});
+
+function flattenSources(sources) {
+  const visibleSources = sources.filter(source => source.isVisible);
+  return [
+    visibleSources.length,
+    ...visibleSources
+      .flatMap(source => [
+        source.position.x,
+        source.position.y,
+        source.intensity,
+      ])
+  ]
+}
+
+function processFrame() {
+  const sources = [lightSource];
+
+  const input = flattenSources(sources);
+  render(input);
+
+  requestAnimationFrame(processFrame);
+}
+
+let reloadTimeout;
+window.addEventListener('resize', () => {
+  clearTimeout(reloadTimeout);
+  reloadTimeout = setTimeout(() => {
+    window.location.reload();
+  }, 100);
+});
 
 function draw() {
-  ctx.fillStyle = '#333';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
-  requestAnimationFrame(draw);
+  requestAnimationFrame(processFrame);
 }
 
 draw(); 
